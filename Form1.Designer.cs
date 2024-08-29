c267ac95b732d9af33f6ac740ec43870bc51195e
hustler <hustler@LAPTOP-PFKFF0KJ>
2024-08-30 01:00:46 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAddTasks = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.buttonClearAll = new System.Windows.Forms.Button();
+            this.buttonUncheck = new System.Windows.Forms.Button();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To Do List";
+            // 
+            // textBoxAddTasks
+            // 
+            this.textBoxAddTasks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBoxAddTasks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxAddTasks.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAddTasks.Location = new System.Drawing.Point(42, 97);
+            this.textBoxAddTasks.Name = "textBoxAddTasks";
+            this.textBoxAddTasks.Size = new System.Drawing.Size(152, 36);
+            this.textBoxAddTasks.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonAdd.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(200, 97);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(98, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Add a Task";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonRemove.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRemove.Location = new System.Drawing.Point(200, 126);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(98, 25);
+            this.buttonRemove.TabIndex = 5;
+            this.buttonRemove.Text = "Remove Task";
+            this.buttonRemove.UseVisualStyleBackColor = false;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.checkedListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.checkedListBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.checkedListBox.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(42, 142);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(152, 126);
+            this.checkedListBox.TabIndex = 6;
+            // 
+            // buttonClearAll
+            // 
+            this.buttonClearAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonClearAll.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClearAll.Location = new System.Drawing.Point(200, 225);
+            this.buttonClearAll.Name = "buttonClearAll";
+            this.buttonClearAll.Size = new System.Drawing.Size(98, 23);
+            this.buttonClearAll.TabIndex = 7;
+            this.buttonClearAll.Text = "Clear All";
+            this.buttonClearAll.UseVisualStyleBackColor = false;
+            this.buttonClearAll.Click += new System.EventHandler(this.buttonClearAll_Click);
+            // 
+            // buttonUncheck
+            // 
+            this.buttonUncheck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonUncheck.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUncheck.Location = new System.Drawing.Point(200, 186);
+            this.buttonUncheck.Name = "buttonUncheck";
+            this.buttonUncheck.Size = new System.Drawing.Size(98, 33);
+            this.buttonUncheck.TabIndex = 8;
+            this.buttonUncheck.Text = "Uncheck All TAsks";
+            this.buttonUncheck.UseVisualStyleBackColor = false;
+            this.buttonUncheck.Click += new System.EventHandler(this.buttonUncheck_Click);
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonCheck.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCheck.Location = new System.Drawing.Point(200, 157);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(98, 23);
+            this.buttonCheck.TabIndex = 9;
+            this.buttonCheck.Text = "Check All";
+            this.buttonCheck.UseVisualStyleBackColor = false;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonExit.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(201, 254);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(97, 25);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "Close List";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(433, 325);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.buttonUncheck);
+            this.Controls.Add(this.buttonClearAll);
+            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxAddTasks);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAddTasks;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.Button buttonClearAll;
+        private System.Windows.Forms.Button buttonUncheck;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (textBoxAddTasks.Text == "")
+                return;
+            else
+                checkedListBox.Items.Add(textBoxAddTasks.Text);
+                textBoxAddTasks.ResetText();
+        }
+                
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            for (int i = checkedListBox.Items.Count - 1; i >= 0; i--)
+            {
+                if (checkedListBox.GetItemChecked(i))
+                {
+                    checkedListBox.Items.RemoveAt(i);
+
+                }
+
+            }
+
+        }
+                
+        private void Form1_Load(object sender, EventArgs e)
+        {
+  
+           
+        }
+
+        private void buttonClearAll_Click(object sender, EventArgs e)
+        {
+            checkedListBox.Items.Clear();
+            textBoxAddTasks.ResetText();
+        }
+
+        private void buttonUncheck_Click(object sender, EventArgs e)
+        {
+            for (int a = 0; a < checkedListBox.Items.Count; a++)
+                checkedListBox.SetItemChecked(a, false);
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            for (int a = 0; a < checkedListBox.Items.Count; a++)
+                checkedListBox.SetItemChecked(a, true);
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
